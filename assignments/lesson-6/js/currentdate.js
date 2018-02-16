0cee49ac7bc6a83b013efbb01d26442c4a715215
Ali Cope <35251289+Cope-Ali@users.noreply.github.com>
2018-02-15 17:16:23 -0800

    assignment 6

---
+++ assignments/lesson-6/js/currentdate.js
@@ -0,0 +1,68 @@
+var day;
+switch (new Date().getDay()) {
+    case 0:
+        day = "Sunday, ";
+        break;
+    case 1:
+        day = "Monday, ";
+        break;
+    case 2:
+        day = "Tuesday, ";
+        break;
+    case 3:
+        day = "Wednesday, ";
+        break;
+    case 4:
+        day = "Thursday, ";
+        break;
+    case 5:
+        day = "Friday, ";
+        break;
+    case 6:
+        day = "Saturday, ";
+        break;
+};
+var date = new Date().getDate();
+var month;
+switch (new Date().getMonth()) {
+    case 0:
+        month = " January ";
+        break;
+    case 1:
+        month = " February ";
+        break;
+    case 2:
+        month = " March ";
+        break;
+    case 3:
+        month = " April ";
+        break;
+    case 4:
+        month = " May ";
+        break;
+    case 5:
+        month = " June ";
+        break;
+    case 6:
+        month = " July ";
+        break;
+    case 7:
+        month = " August ";
+        break;
+    case 8:
+        month = " September ";
+        break;
+    case 9:
+        month = " October ";
+        break;
+    case 10:
+        month = " November ";
+        break;
+    case 11:
+        month = " December ";
+        break;
+};
+var year = new Date().getFullYear();
+console.log(day, date, month, year)
+var dateString = day + date + month + year;
+document.getElementById("currentdate").innerHTML = dateString;
